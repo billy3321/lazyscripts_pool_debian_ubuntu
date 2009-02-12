44101e49e420c5c924b7d3ab5545fdc78183b355
billy3321 <billy3321@sonnet77-desktop.(none)>
2009-02-13 00:19:49 +0800

    Add some scripts from list.xml of lazybuntu to Networking and Localization.

---
+++ Networking/amule.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Last Modified: 2 Dec 2008
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW '安裝 aMule P2P 軟體 - 在 Linux 下養騾子'
+# @desc_zhTW 'aMule 是由 Windows 下 eMule 移植而來的跨平台軟體。'
+# @category 'Networking'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @author '2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>'
+# @license 'GPL'
+# @ubuntu 
+# @debian
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install amule
+
