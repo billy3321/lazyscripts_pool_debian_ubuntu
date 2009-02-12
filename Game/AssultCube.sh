a9a693e0da009b9250942ea748e77558cf3c7972
billy3321 <billy3321@sonnet77-desktop.(none)>
2009-02-13 01:10:07 +0800

    Add scripts from game.xml of lazybuntu to Game.

---
+++ Game/AssultCube.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Last Modified: 2 Dec 2008
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW '安裝 AssultCube (需要 3D 加速)'
+# @desc_zhTW 'Linux 下相當類似 CS 的遊戲，3D 畫面逼真、流暢，
+#             硬體需求並不高，遊戲的檔案也不大，可多人連線對戰，
+#             完全正版，完全免費，強力推薦！'
+# @category 'Game'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @author '2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>'
+# @license 'GPL'
+# @ubuntu 
+# @debian
+# @platform 'i386 amd64'
+
+scripts/getdeb.py AssaultCube 1633
+
---
+++ Game/Dosbox.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Last Modified: 2 Dec 2008
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW 'Dosbox'
+# @desc_zhTW 'MS-DOS 模擬器，讓你在 Linux 下玩古董級的 DOS 遊戲。'
+# @category 'Game'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @author '2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>'
+# @license 'GPL'
+# @ubuntu 
+# @debian
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install dosbox
+
