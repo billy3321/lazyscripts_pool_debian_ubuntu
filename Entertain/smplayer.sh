53ae3c2295deff4041e8db6f50aabdc87b172878
billy3321 <billy3321@sonnet77-desktop.(none)>
2009-02-13 00:36:53 +0800

    Add scripts from list.xml to Entertain and Multimedia

---
+++ Entertain/smplayer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Last Modified: 2 Dec 2008
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW '安裝 SMplayer - Mplayer 的改良式介面'
+# @desc_zhTW '有鑑於 Mplayer 官方的操作介面不太方便，有人做了更好的圖形介面出來。
+#             SMPlayer 的操作介面相當簡單、直觀，相當推薦使用。'
+# @category 'Entertain'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @author '2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>'
+# @license 'GPL'
+# @ubuntu 
+# @debian
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install smplayer
+
