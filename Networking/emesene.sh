483e5c8f8f25132f451b72832d67b075b6622f2f
billy3321 <billy3321@gmail.com>
2009-04-01 22:30:42 +0800

    Add emesene.sh

---
+++ Networking/emesene.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+
+#
+# @name_enUS '' 
+# @name_zhTW '安裝 emesene - Windows Live Messenger Client 端軟體'
+# @desc_enUS ''
+# @desc_zhTW '若您時常使用 Windows Live Messenger 服務的話，emesene是一款好用的Client端軟體，支援離線訊息，還可自訂布景主題。'
+# @category 'Networking'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @author '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @license 'GPL'
+# @debian ''
+# @ubuntu ''
+# @platform 'i386 AMD64'
+
+apt-get install emesene
+
+wget http://emesene.org/trac/raw-attachment/wiki/Plugins/LastSaid.py
+
+mv LastSaid.py ${REAL_HOEM}/.config/emesene1.0/
+
