fcf608dc511d342475c1bb4269cfbfb192f54bb4
billy3321 <billy3321@gmail.com>
2009-04-03 00:04:00 +0800

    Add droidttf.sh

---
+++ Localization/droidttf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+#
+# @name_enUS '' 
+# @name_zhTW '安裝 Droid 字形 
+# @desc_enUS ''
+# @desc_zhTW 'Droid 字形是 Google 開發用來在Android手機上使用的字形，佔用空間小，而外型也很清晰。'
+# @category 'Localization'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <bill3321 -AT- gmail.com>'
+# @author '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <bill3321 -AT- gmail.com>'
+# @license 'GPL'
+# @debian ''
+# @ubuntu ''
+# @platform 'i386 AMD64'
+# @child 'Common/add-apt-sources'
+
+
+. add-apt-sources
+
+add_ubuntu_fonts()
+
+sudo apt-get install ttf-droid
+
