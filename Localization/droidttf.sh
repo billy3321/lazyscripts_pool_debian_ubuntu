d2201d5d8335735112a04e2da54af5ed79678847
Yuren Ju <yurenju@yurenju-laptop.(none)>
2009-04-05 12:07:12 +0800

    * fix droid font can't install on debian bug

--- Localization/droidttf.sh
+++ Localization/droidttf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 # -*- coding: utf-8 -*-
 #
-# @name_enUS '' 
+# @name_enUS 'install Droid ttf fonts' 
 # @name_zhTW '安裝 Droid 字形 
-# @desc_enUS ''
+# @desc_enUS 'Droid font is used on Google Android Platform, it's small and clear'
 # @desc_zhTW 'Droid 字形是 Google 開發用來在Android手機上使用的字形，佔用空間小，而外型也很清晰。'
 # @category 'Localization'
 # @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <bill3321 -AT- gmail.com>'
