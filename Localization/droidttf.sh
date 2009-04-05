5a70f77e2a41a9ef39db15c6ec110c5dc253821f
Yuren Ju <yurenju@yurenju-laptop.(none)>
2009-04-05 12:22:56 +0800

    * fix minor bug on droidttf.sh

--- Localization/droidttf.sh
+++ Localization/droidttf.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # @name_enUS 'install Droid ttf fonts' 
-# @name_zhTW '安裝 Droid 字形 
+# @name_zhTW '安裝 Droid 字形'
 # @desc_enUS 'Droid font is used on Google Android Platform, it's small and clear'
 # @desc_zhTW 'Droid 字形是 Google 開發用來在Android手機上使用的字形，佔用空間小，而外型也很清晰。'
 # @category 'Localization'
