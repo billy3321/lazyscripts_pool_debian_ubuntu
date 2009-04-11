07974958aee0df5efa19519e7c328605d162ff50
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-11 21:09:17 +0800

    Add Htop intstall Script

---
+++ Customize/htop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install Htop'
+# @name_zhTW '安裝 Htop 系統行程管理工具'
+# @desc_enUS ''
+# @desc_zhTW 'Htop 是一個輕量化的系統行程管理工具，簡單的介面可以清楚的瞭解目前系統行程狀態，是除了系統預設行程管理的另一個不錯的行程。'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Customize'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install htop
+
+#END
