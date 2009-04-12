6223e0cf888bb6a831c7a394d72df1197c0c7921
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-12 23:00:51 +0800

    Add PCMan File Manager

---
+++ Customize/pcmanfm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install PCMan File Manager'
+# @name_zhTW '安裝 PCMan File Manager'
+# @desc_enUS ''
+# @desc_zhTW '由PCMan所開發的檔案總管系統，有輕巧、方便等優點。'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Customize'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install pcmanfm
+
+#END
