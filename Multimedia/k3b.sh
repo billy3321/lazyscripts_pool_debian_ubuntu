00b72bf601b2296736ef82293ed8e3a70836d1af
billy3321 <billy3321@gmail.com>
2009-04-12 20:00:57 +0800

    Add k3b install

---
+++ Multimedia/k3b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Last Modified: 12 Apr 2009
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW '安裝 K3b 燒錄軟體'
+# @desc_zhTW '燒錄光碟的好用軟體。'
+# @category 'Multimedia'
+# @maintaner '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @author '林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>'
+# @license 'GPL'
+# @ubuntu 
+# @debian
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install k3b
+
