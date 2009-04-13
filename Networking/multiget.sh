da4c36d0ecc08c66e67fa4a66edc175c55975bdc
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-13 15:01:57 +0800

    Fix MultiGet can't install problem (Ubuntu)

--- Networking/multiget.sh
+++ Networking/multiget.sh
@@ -30,7 +30,7 @@ case $DISTRIB_ID in
 		esac
 		;;
     "Ubuntu")
-	apt-get --force-yes -y install multiget
+	./download-install MultiGet "http://ncu.dl.sourceforge.net/sourceforge/multiget/multiget_1.1.2-0getdeb1_i386.deb"
 	;;
 	*)
 	echo "Sorry, Lazyscripts not support $DISTRIB_ID"
