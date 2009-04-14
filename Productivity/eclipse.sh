7f77e2fe1046f330df05168e3c03cfe8fe129fb5
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-14 21:04:07 +0800

    Add Eclipse IDE

---
+++ Productivity/eclipse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install Eclipse'
+# @name_zhTW '安裝 Eclipse'
+# @desc_enUS ''
+# @desc_zhTW '一個跨平台的程式撰寫工具。'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Productivity'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+echo "安裝 Eclipse"
+
+apt-get -y --force-yes  install eclipse
+
+#END
