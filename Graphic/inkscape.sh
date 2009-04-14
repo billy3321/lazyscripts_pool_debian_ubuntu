036120052f05b010bccc91f569d551352a6eea45
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-14 23:09:31 +0800

    Add Inkscape's intro

---
+++ Graphic/inkscape.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install Inkscape'
+# @name_zhTW '安裝 Inkscape 向量繪畫軟體'
+# @desc_enUS ''
+# @desc_zhTW 'Inkscape 是一個向量繪圖軟體，支援SVG格式。( SVG 是 W3C 的標準格式 )'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category ''
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install inkscape
+#END
