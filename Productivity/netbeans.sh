df69964dc8c3dc34e0d5c1dcf4a40f54051b8d7a
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-17 21:24:01 +0800

    Add NetBeans IDE

---
+++ Productivity/netbeans.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install NetBeans'
+# @name_zhTW '安裝 NetBeans'
+# @desc_enUS ''
+# @desc_zhTW '一個由Sun(昇陽)所發展出來的跨平台Java開發平台，目前支援許多不同語言。Ex: C/C++,Ruby...'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Productivity'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+echo "安裝 Netbeans"
+
+apt-get -y --force-yes  install netbeans
+
+#END
