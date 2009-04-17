6a05b4edecfb21ee1df55c63dd707058620f0923
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-17 21:36:43 +0800

    Add Brasero

---
+++ Multimedia/brasero.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install Brasero'
+# @name_zhTW '安裝 Brasero 光碟燒錄工具'
+# @desc_enUS 'Brasero is a application to burn CD/DVD for Gnome Desktop.'
+# @desc_zhTW '一個簡單的光碟燒錄工具。'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Multimedia'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+echo "Install Brasero."
+
+apt-get -y --force-yes  install brasero
+
+#END
