68ff6c5cacd37c782df24e1505207b5a539f3f73
c9s <cornelius.howl@gmail.com>
2009-04-24 01:27:20 +0800

    provide vim install script.

---
+++ Development/vim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# GPL
+#
+# @name_enUS 'Install Vim'
+# @name_zhTW '安裝 Vim'
+# @desc_enUS 'Vim Editor'  
+# @desc_zhTW 'Vim 編輯器'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category  'Development'
+# @maintaner '林佑安 c9s (Cornelius) <cornelius.howl _at_ gmail.com>'
+# @author    '林佑安 c9s (Cornelius) <cornelius.howl _at_ gmail.com>'
+# @license   'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+echo "安裝 Vim"
+
+apt-get install vim-full -y
+
+# XXX: make default vimrc
+
+#END
