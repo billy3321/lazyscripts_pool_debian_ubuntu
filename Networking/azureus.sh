e6ea4f921bd35cd26a7b796e37898348d95a5769
Kang Min Wang <aminzai@Aminzai-Desktop.home.aminzai.net>
2009-04-23 23:36:45 +0800

    Add Azureus BT Client

---
+++ Networking/azureus.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install Azureus Bittorrent Client'
+# @name_zhTW '安裝 Azureus BitTorrent 下載工具'
+# @desc_enUS 'Free java Bittorrent client formerly known as Azureus. '
+# @desc_zhTW '一個由JAVA撰寫而成的BT下載工具，具有跨平台的特性。'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Customize'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+apt-get -y --force-yes install vuze
+
+#END
