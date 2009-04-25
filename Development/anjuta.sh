a73f110b3b1138b491a7e2a1576ee00d77b45c2d
“Aminzai” <aminzai@Aminzai-Laptop.laptop.aminzai.net>
2009-04-25 21:24:14 +0800

    Add anjuta

---
+++ Development/anjuta.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>
+#
+# @name_enUS 'Install Anjuta IDE'
+# @name_zhTW '安裝 Anjuta IDE'
+# @desc_enUS 'A GNOME development IDE, for C/C++'
+# @desc_zhTW '一個好用的 C/C++ IDE'
+# @warn_enUS ''
+# @warn_zhTW ''
+# @category 'Development'
+# @maintaner '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @author '王綱民 Kang-Min Wang (Aminzai,阿民) <lagunawang -AT- gmail.com>'
+# @license 'GPL'
+# @debian
+# @ubuntu 
+# @platform 'i386 amd64'
+
+echo "Install Anjuta IDE"
+
+apt-get install -y --force-yes anjuta
+
+#END
